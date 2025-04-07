84e2e06940157551b73e2215a9fed4a126582024
Fraser Callaghan <callaghan.fm@gmail.com>
2025-04-07 10:51:45 +0200

    update some paths for docs

--- source/conf.py
+++ source/conf.py
@@ -12,7 +12,7 @@
 #
 import os
 import sys
-sys.path.insert(0, os.path.abspath('./hurahura'))
+sys.path.insert(0, os.path.abspath('..'))
 
 
 # -- Project information -----------------------------------------------------
